bd2f8900085340c879364602d243e3a5237d2ca5
Yoi-Nami-Ra <yoinamira@gmail.com>
2011-05-02 00:12:01 +0200

    some first draft project

---
+++ code/includes/defs.h
@@ -0,0 +1,9 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+enum errorCode {
+	errOK = 0,
+	errAll
+};
+
+#endif //DEFS_H
---
+++ code/src/main.cpp
@@ -0,0 +1,14 @@
+// Host code
+
+#include <stdlib.h>
+#include "defs.h"
+
+#define Wine_DataSet
+
+errorCode LoadData
+
+int main(int argc, char** argv)
+{
+    shrQAStart(argc, argv);
+
+}
