a7d3bb9ab95d5c5ed05aa78f42efd194c65d5eb0
Yoi-Nami-Ra <yoinamira@gmail.com>
2011-11-17 18:16:22 +0100

    exported code from main so there will be less code copying betwen platforms

---
+++ code/evoEngine.h
@@ -0,0 +1,18 @@
+/**
+ 17/11/2011
+ Jaroslaw Wojtasik
+
+ noCuda
+
+ evoEngine.h
+ **/
+
+#ifndef EVOENGINE_H
+#define EVOENGINE_H
+
+/**
+ * Single function to launch it all.
+ */
+void runEvo( void );
+
+#endif // EVOENGINE_H
